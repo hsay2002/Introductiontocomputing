d7be7730bbfd5b554a4fc789f418453d7c0e3c76
hsay2002 <104245194+hsay2002@users.noreply.github.com>
2022-04-24 23:02:47 +0530

    Add files via upload

---
+++ result.py
@@ -0,0 +1,36 @@
+enter first no. : 78
+enter second no. :45
+enter third no. :98
+Average of the given numbers:  73.66666666666667
+Enter your gross income(in $) :50000
+Enter the number of dependents in your family : 4
+Taxable income:  37000.0
+Tax 7400.0
+Enter number of seconds: 789
+Given seconds converts to : 13 minutes 9 seconds
+Addition : 75
+cosine( 0 ) is 1.0 sine( 0 ) is 0.0
+cosine( 15 ) is 0.9659 sine( 15 ) is 0.2588
+cosine( 30 ) is 0.866 sine( 30 ) is 0.5
+cosine( 45 ) is 0.7071 sine( 45 ) is 0.7071
+cosine( 60 ) is 0.5 sine( 60 ) is 0.866
+cosine( 75 ) is 0.2588 sine( 75 ) is 0.9659
+cosine( 90 ) is 0.0 sine( 90 ) is 1.0
+cosine( 105 ) is -0.2588 sine( 105 ) is 0.9659
+cosine( 120 ) is -0.5 sine( 120 ) is 0.866
+cosine( 135 ) is -0.7071 sine( 135 ) is 0.7071
+cosine( 150 ) is -0.866 sine( 150 ) is 0.5
+cosine( 165 ) is -0.9659 sine( 165 ) is 0.2588
+cosine( 180 ) is -1.0 sine( 180 ) is 0.0
+cosine( 195 ) is -0.9659 sine( 195 ) is -0.2588
+cosine( 210 ) is -0.866 sine( 210 ) is -0.5
+cosine( 225 ) is -0.7071 sine( 225 ) is -0.7071
+cosine( 240 ) is -0.5 sine( 240 ) is -0.866
+cosine( 255 ) is -0.2588 sine( 255 ) is -0.9659
+cosine( 270 ) is -0.0 sine( 270 ) is -1.0
+cosine( 285 ) is 0.2588 sine( 285 ) is -0.9659
+cosine( 300 ) is 0.5 sine( 300 ) is -0.866
+cosine( 315 ) is 0.7071 sine( 315 ) is -0.7071
+cosine( 330 ) is 0.866 sine( 330 ) is -0.5
+cosine( 345 ) is 0.9659 sine( 345 ) is -0.2588
+>
