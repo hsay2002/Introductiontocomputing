963f41908fa0260653395b7779124fa4471443cf
hsay2002 <104245194+hsay2002@users.noreply.github.com>
2022-05-06 11:19:14 -0700

    Add files via upload

---
+++ assign2_solution.py
@@ -0,0 +1,38 @@
+A) Length of the given string:  35 
+
+B) Reverse of the given string is :
+egaugnal evitisnes esac a si nohtyP 
+
+C) New sliced string is:  
+ a case sensitive 
+
+D) String after replacing:  
+ Python is object oriented language 
+
+E) Index of substring 'a' is: 10 
+
+F) Sting without white spaces:  
+ Pythonisacasesensitivelanguage
+Enter your name :	Yash Vashishth
+Enter your SID :	21107083
+Enter your CGPA:	9.0
+ENter your department name :	Mechanical
+Hey, Yash Vashishth Here!
+My SID is 21107083 
+I am from Mechanical department and my CGPA is 9.0
+A. a & b = 8
+B. a | b = 58
+C. a ^ b = 50
+D. a << 2 =  224
+E. a >> 2 =  14  and  b >> 4 =  0
+Enter any string: My name is yash
+Yes
+Enter First length : 15
+Enter Second length : 25
+Enter Third length : 35
+The triangle can be formed?
+Yes!
+enter number 1:3
+enter number 2:4
+3
+> 
