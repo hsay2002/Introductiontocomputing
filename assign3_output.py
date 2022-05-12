294d96330be0a5365f4ff13945da1c19f68f42a1
hsay2002 <104245194+hsay2002@users.noreply.github.com>
2022-05-12 09:03:30 -0700

    Add files via upload

---
+++ assign3_output.py
@@ -0,0 +1,52 @@
+Ans-1 :-
+Enter a number: 56
+Binary equivalent of 56 is 111000
+Ans-2 :-
+Enter expression: ((45*5+89-67)/5)**2
+((45*5+89-67)/5)**2 = 2440.3599999999997
+Ans-3:-
+a) (3+4)(5) =  35
+Enter an integer = 4
+b) 6.0
+Enter radius =7
+c) 615.7521601035994
+Enter angle 1 = 56
+Enter angle 2 = 45
+d) 3.188126334072579
+Enter x coordinate of first point: 4
+Enter y coordinate of first point: 6
+Enter x coordinate of second point: 8
+Enter y coordinate of second point: 9
+e) 0.75
+Ans-4 :-
+4.(a)
+0
+1
+2
+3
+4
+4.(b)
+3
+4
+5
+6
+7
+8
+9
+4.(c)
+4
+7
+10
+4.(d)
+15
+13
+11
+9
+7
+4.(e)
+Ans-5:-
+Enter number of carbon atoms in molecule: 4
+Enter number of hydrogen atoms in molecule: 10
+Enter number of oxygen atoms in molecule: 2
+The molecular weight of the compound is 90.121
+> 
