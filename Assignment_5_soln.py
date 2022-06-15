884a4135a5a0e5cc55fd82fd8099abe00dfd56e6
hsay2002 <104245194+hsay2002@users.noreply.github.com>
2022-06-15 10:50:19 -0700

    Create Assignment_5_soln.py

---
+++ Assignment_5_soln.py
@@ -0,0 +1,74 @@
+Enter your string here : My name is Yash Vashishth
+The reversed string is :  hthsihsaV hsaY si eman yM
+ Enter the lower range limit: 12
+ Enter the upper range limit: 100
+ Enter the number to be divided by: 13
+ 13
+26
+39
+52
+65
+78
+91
+Enter the first side: 8
+Enter the second side : 6
+Enter the third side : 10
+Area of the triangle:  24.0
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+Enter Right Triangle Consecutive Alphabets Rows = 5
+====The Right Triangle of Consecutive Alphabets Pattern====
+A 
+B C 
+D E F 
+G H I J 
+K L M N O 
+Please, Enter the Lowest Range Value: 12
+Please, Enter the Upper Range Value: 97
+The Prime Numbers in the range are: 
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
+77,154,231,308,385,462
+ Enter the numbers: 1
+ Enter the numbers: 2
+ Enter the numbers: 0
+ Enter the numbers: 0
+ Enter the numbers: 9
+ Enter the numbers: 7
+ Enter the numbers: 8
+ Enter the numbers: 6
+ Enter the numbers: 5
+ Enter the numbers: 4
+ [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+[]
+[0, 2, 4, 6, 8]
+[1, 3, 5, 7, 9]
+{1: 1, 2: 1, 0: 2, 9: 1, 7: 1, 8: 1, 6: 1, 5: 1, 4: 1}
+{'Python': 1, 'is': 1, 'a': 1, 'case': 1, 'sensitive': 1, 'language.': 1}
+> 
