f528a60995199be694b0ac11ca668d3ef5842dbf
hsay2002 <104245194+hsay2002@users.noreply.github.com>
2022-09-25 23:24:11 +0530

    Create dsa_assign1_ans.py

---
+++ dsa_assign1_ans.py
@@ -0,0 +1,23 @@
+Enter the no of family members:
+4
+Enter the name of family member:
+Ram
+Enter the age of family member:
+23
+Enter the name of family member:
+Shyam
+Enter the age of family member:
+56
+Enter the name of family member:
+Rani
+Enter the age of family member:
+19
+Enter the name of family member:
+Kashyap
+Enter the age of family member:
+32
+|Name:  Shyam  age:  56 |  ---><------ |Name:  Kashyap  age:  32 |  ---><------ |Name:  Ram  age:  23 |  ---><------ |Name:  Rani  age:  19 |  ---><------ None
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
